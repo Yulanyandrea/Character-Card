b9df3db57b341475a8caeead2c8f111d8dc42721
Yulany Munevar <andra_852@hotmail.com>
2022-10-27 10:33:27 -0500

    rick-and-morty-react-card

---
+++ character-card/src/App.js
@@ -0,0 +1,27 @@
+import CharacterCard from "./components/CharacterCard";
+
+import data from "./assets/data.json";
+
+import "./App.css";
+
+function App() {
+ 
+  return (
+    <div className="app">
+      <h1>Rick and Morty Characters</h1>
+      <h2>Your code here</h2>
+
+      <div className="container">
+        {
+           data.map(character =>{
+            return <CharacterCard key={character.id} name={character.name} image={character.image} status={character.status} species={character.species} locationUrl={character.location.url} locationName={character.location.name}
+            originUrl={character.origin.url} originName={character.origin.name}/>
+          })
+        }
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
